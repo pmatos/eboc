0f33f247dff89e054dd6ce1c54127a2a92f9a279
Paulo J. Matos <pocmatos@gmail.com>
2010-06-13 16:23:58 +0100

    Added new serialize-utils with deserialize-assoc.

---
+++ modes/esmc-mode/serialize-utils.scm
@@ -0,0 +1,9 @@
+#lang scheme
+
+(require scheme/serialize)
+
+(define (deserialize-assoc assl)
+  (map (lambda (p)
+         (cons (deserialize  (car p))
+               (deserialize (cdr p))))
+       assl))
